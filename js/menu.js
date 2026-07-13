11bcd92e6f7420c0f4d53db59d52e34fa263ddfb
piknikkutusu <piknikkutusu1@gmail.com>
2026-07-14 01:43:43 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+    const menu = await loadMenu();
+
+    console.log(menu);
+
+    const waffleBtn = document.getElementById("waffleBtn");
+    const drinkBtn = document.getElementById("drinkBtn");
+
+    const waffleCount = menu.filter(x => x.kategori === "Waffle").length;
+    const drinkCount = menu.filter(x => x.kategori === "İçecek").length;
+
+    waffleBtn.querySelector("p").innerText =
+        waffleCount + " ürün mevcut";
+
+    drinkBtn.querySelector("p").innerText =
+        drinkCount + " ürün mevcut";
+
+    waffleBtn.onclick = () => {
+
+        location.href =
+            "mutfak.html?kategori=Waffle";
+
+    };
+
+    drinkBtn.onclick = () => {
+
+        location.href =
+            "mutfak.html?kategori=İçecek";
+
+    };
+
+});
